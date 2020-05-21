a8828e4174e689e725ddc1a23b2edfbd69300f80
Calvin Tennant <calvintennant@gmail.com>
2020-05-21 13:20:32 -0400

    require dependencies

--- includes/class-tatsu-fontawesome-icons-activator.php
+++ includes/class-tatsu-fontawesome-icons-activator.php
@@ -22,15 +22,27 @@
  */
 class Tatsu_Fontawesome_Icons_Activator {
 
-	/**
-	 * Short Description. (use period)
-	 *
-	 * Long Description.
-	 *
-	 * @since    1.0.0
-	 */
-	public static function activate() {
+  /**
+   * Short Description. (use period)
+   *
+   * Long Description.
+   *
+   * @since    1.0.0
+   */
+  public static function activate() {
 
-	}
+    if (!function_exists('run_tatsu')) {
+      deactivate_plugins(plugin_basename( __FILE__ ));
+      wp_die( __( 'Please install and Tatsu.', 'tatsu-addon-slug'), 'Plugin dependency check', array('back_link' => true));
+    }
+
+
+    // The namespace for FontAwesome is in-fact FortAwesome
+    if (!function_exists( 'FortAwesome\font_awesome_load')) {
+      deactivate_plugins(plugin_basename( __FILE__ ));
+      wp_die( __( 'Please install and Font Awesome.', 'fontawesome-addon-slug'), 'Plugin dependency check', array('back_link' => true));
+    }
+
+  }
 
 }
